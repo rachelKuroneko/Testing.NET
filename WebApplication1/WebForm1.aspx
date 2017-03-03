0273095d51725a95188cd20b1261faf8609c3464
rachelKuroneko <jingyi-tan@hotmail.com>
2017-03-03 16:00:08 +1000

    initial add

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Welcome to my Life.</title>
+</head>
+<body>
+    <div>Welcome to Practical 1.</div> 
+    <br />
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Calculate" runat="server" Text="Calculate" OnClick="Calculate_Click" /> &emsp;
+            
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox> &emsp;
+            
+            <asp:DropDownList ID="DropDownList1" runat="server">
+                <asp:ListItem Value="0">+</asp:ListItem>
+                <asp:ListItem Value="1">-</asp:ListItem>
+                <asp:ListItem Value="2">*</asp:ListItem>
+                <asp:ListItem Value="3">/</asp:ListItem>
+            </asp:DropDownList> &emsp;
+            
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox> &emsp;
+            
+            <asp:Label ID="Label1" runat="server" Text="=Base10;"></asp:Label> &emsp;
+            
+            <asp:TextBox ID="base10" runat="server"></asp:TextBox> &emsp;
+
+            <asp:Label ID="Label2" runat="server" Text="=Base2;"></asp:Label> &emsp;
+
+            <asp:TextBox ID="base2" runat="server"></asp:TextBox> 
+            <br />
+        </div>
+        
+        <div>
+            <br />
+            <asp:Button ID="Count" runat="server" Text="Count" OnClick="Count_Click" />
+            
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Number of 0"></asp:Label> &emsp;
+            
+            <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Number of 1"></asp:Label> &emsp;
+   
+            <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>    
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // IsPostBack tells whether page is on its initial load, it prevents duplicate
+            if (!IsPostBack) { }
+        }
+
+        protected void Calculate_Click(object sender, EventArgs e)
+        {
+            var input1 = int.Parse(TextBox1.Text);
+            var input2 = int.Parse(TextBox3.Text);
+
+            if (DropDownList1.SelectedIndex == 0)
+            {
+                int addition = input1 + input2;
+                base10.Text = addition.ToString();
+                base2.Text = Convert.ToString(addition, 2);
+            }
+
+            else if (DropDownList1.SelectedIndex == 1)
+            {
+                int subtract = input1 - input2;
+                base10.Text = subtract.ToString();
+                base2.Text = Convert.ToString(subtract, 2);
+            }
+
+            else if (DropDownList1.SelectedIndex == 2)
+            {
+                int multiply = input1 * input2;
+                base10.Text = multiply.ToString();
+                base2.Text = Convert.ToString(multiply, 2);
+            }
+            else
+            {
+                try
+                {
+                    int divide = input1 / input2;
+                    base10.Text = divide.ToString();
+                    base2.Text = Convert.ToString(divide, 2);
+                }
+                catch(System.DivideByZeroException)
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "Script",
+                        "<script>alert('Any number divided by {0} is undefined. Please key in other value.');</script>");
+                }
+            }
+        }
+
+        
+        protected void Count_Click(object sender, EventArgs e)
+        {
+            int numOfZero = base2.Text.Split('0').Length - 1;
+            int numOfOne = base2.Text.Split('1').Length - 1;
+
+            TextBox5.Text = numOfZero.ToString();
+            TextBox6.Text = numOfOne.ToString();
+        }
+    }
+}
